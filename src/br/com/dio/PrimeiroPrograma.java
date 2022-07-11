80d2805b4a42bde5dafb944876a79a8cb80bfe07
fermarconato <fernanda.marconato@gmail.com>
2022-07-11 10:15:23 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        gato Gato = new gato();
+        System.out.println(Gato);
+
+    }
+}
